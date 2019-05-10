36202842f3d96ca044511ae491c833f7f7282c72
Viktor <barta.viktor.ufs@gmail.com>
2019-05-10 16:58:21 +0200

    feat(shoppingcart) add items to shopping cart

--- public/script.js
+++ public/script.js
@@ -1,1 +1,32 @@
-console.log("It works!");
+new Vue({
+	el: "#app",
+	data: {
+		total: 0,
+		products: [
+			{ title: "Product 1", id: 1, price: 9.99 },
+			{ title: "Product 2", id: 2, price: 10.99 },
+			{ title: "Product 3", id: 3, price: 11.99 },
+		],
+		cart: []
+	},
+	methods: {
+		addToCart: function (product) {
+			this.total += product.price;
+			let found = false;
+			for (let i = 0; i < this.cart.length; i++) {
+				if(this.cart[i].id === product.id) {
+					this.cart[i].qty++;
+					found = true;
+				}
+			}
+			if(!found) {
+				this.cart.push({
+					id: product.id,
+					title: product.title,
+					price: product.price,
+					qty: 1
+				});
+			}
+		},
+	}
+});
