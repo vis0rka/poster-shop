287795177162ef02f0b427fa0978761d8810a436
Viktor <barta.viktor.ufs@gmail.com>
2019-05-10 19:47:26 +0200

    feat(scroll) when the user scroll add more products

--- public/script.js
+++ public/script.js
@@ -1,3 +1,6 @@
+let load_num = 4;
+let watcher;
+
 new Vue({
 	el: "#app",
 	data: {
@@ -7,18 +10,19 @@ new Vue({
 		search: "cat",
 		lastSearch: "",
 		loading: false,
+		results: [],
 	},
 	methods: {
 		addToCart: function (product) {
 			this.total += product.price;
 			let found = false;
 			for (let i = 0; i < this.cart.length; i++) {
-				if(this.cart[i].id === product.id) {
+				if (this.cart[i].id === product.id) {
 					this.cart[i].qty++;
 					found = true;
 				}
 			}
-			if(!found) {
+			if (!found) {
 				this.cart.push({
 					id: product.id,
 					title: product.title,
@@ -27,24 +31,47 @@ new Vue({
 				});
 			}
 		},
-		onSubmit: function() {
+		onSubmit: function () {
 			this.products = [];
+			this.results = [];
 			this.loading = true;
 			let path = `/search?q=${this.search}`;
 			this.$http.get(path)
-			.then(response => {
-					this.products = response.body;
+				.then(response => {
+					this.results = response.body;
 					this.lastSearch = this.search;
+					this.appendResult();
 					this.loading = false;
-			})
+				})
 		},
+		appendResult: function () {
+			if (this.products.length < this.results.length) {
+				let toAppend = this.results.slice(
+					this.products.length,
+					load_num + this.products.length
+				);
+				this.products = this.products.concat(toAppend)
+			}
+		}
 	},
 	filters: {
-		currency: function(price) {
+		currency: function (price) {
 			return `$${price.toFixed(2)}`
 		}
 	},
-	created: function() {
+	created: function () {
 		this.onSubmit();
-	}
+	},
+	updated() {
+		let sensor = document.querySelector("#product-list-bottom");
+		watcher = scrollMonitor.create(sensor);
+		watcher.enterViewport(this.appendResult);
+	},
+	beforeUpdate() {
+		if (watcher) {
+			watcher.destroy();
+			watcher = null;
+		}
+	},
 });
+
